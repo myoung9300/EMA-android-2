364e995035461cc8bbb97ecf0f9806ffa24b3186
Myoung9300 <myoung9300@icloud.com>
2023-02-07 15:26:02 -0700

    update (2/7)

--- components/Pdf.js
+++ components/Pdf.js
@@ -1,11 +1,11 @@
 import { ActivityIndicator, SafeAreaView } from "react-native";
 import React from "react";
-import WebView from "react-native-webview";
+import PDFReader from "rn-pdf-reader-js";
 
 const Pdf = ({ uri }) => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <WebView
+      <PDFReader
         source={{ uri: `https://ema-planner.herokuapp.com/${uri}.pdf` }}
         startInLoadingState={true}
         renderLoading={() => <ActivityIndicator color="black" size="large" />}
